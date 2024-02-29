9f37dbfd239b133a5564fd541167258b1b9c4a25
ZhiyuanSue <2262387848@qq.com>
2024-02-29 21:53:24 +0800

    try to start x86

--- arch/x86_64/boot/boot.s
+++ arch/x86_64/boot/boot.s
@@ -2,9 +2,14 @@
     .section .boot
 _start:
 	jmp multiboot_entry
+    .align 4
 multiboot_header:
-    
-
+    .long   0xE85250D6  /*magic*/
+    .long   0x0         /*archtecture,0 means 32-bit i386*/
+    .long   multiboot_entry-multiboot_header        /*header length*/
+    .long   -(0xE85250D6+0x0+multiboot_entry-multiboot_header)   /*checksum*/
 multiboot_entry:
 	call cmain
-    
+    jmp hlt
+hlt:
+    jmp hlt
