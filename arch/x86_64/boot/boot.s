693027e8721b0479b1642be6b1426710cd5ddf61
ZhiyuanSue <2262387848@qq.com>
2024-03-02 16:17:54 +0800

    del multiboot2 protocol

--- arch/x86_64/boot/boot.s
+++ arch/x86_64/boot/boot.s
@@ -1,18 +1,9 @@
-#include "multiboot2_tag.h"
     .section .boot
     .global _start
     .align 8
 _start:
-	jmp multiboot_entry
-    .align 4
-multiboot_header:
-    .long   0xE85250D6  /*magic*/
-    .long   0x0         /*archtecture,0 means 32-bit i386*/
-    .long   multiboot_header_end-multiboot_header        /*header length*/
-    .long   -(0xE85250D6+0x0+multiboot_header_end-multiboot_header)   /*checksum*/
-
-multiboot_header_end:
-multiboot_entry:
+	jmp entry
+_entry:
     /*set sp*/
 
     /*check magic*/
--- include/arch/x86_64/multiboot2.h
+++
@@ -1,7 +0,0 @@
-#include <shampoos/types.h>
-
-struct multiboot2_tag_address{
-    u32 tag;
-};
-
-
