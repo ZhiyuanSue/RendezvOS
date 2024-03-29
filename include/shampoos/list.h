a13e4c47422753e10f97eec5df6f8d2bb55fed06
ZhiyuanSue <2262387848@qq.com>
2024-03-30 00:26:29 +0800

    try to add list.h

--- include/shampoos/list.h
+++ include/shampoos/list.h
@@ -1,1 +1,36 @@
+#ifndef _SHAMPOOS_LIST_H_
+#define _SHAMPOOS_LIST_H_
 #include <shampoos/types.h>
+
+/* is a simple copy of the linux list.h, just for fun,don't too strict*/
+struct list_entry{
+	struct list_entry *prev, *next;
+};
+
+#define LIST_HEAD_INIT(name) { &(name),&(name) }
+#define LIST_HEAD(name)	\
+	struct list_entry name = LIST_HEAD_INIT(name);
+
+static inline void INIT_LIST_HEAD(struct list_entry *list_node)
+{
+	list_node->prev=list_node;
+	list_node->next=list_node;
+}
+static inline void __list_add(struct list_entry *new_node,
+		struct list_entry *prev,
+		struct list_entry *next)
+{
+	next->prev=new_node;
+	new_node->next=next;
+	new_node->prev=prev;
+	prev->next=new_node;
+}
+static inline void list_add(struct list_entry *new_node,struct list_entry *head)
+{
+	__list_add(new_node,head,head->next);
+}
+static inline void list_add_tail(struct list_entry *new_node,struct list_entry *head)
+{
+	__list_add(new_node,head->prev,head);
+}
+#endif
--- include/shampoos/stddef.h
+++ include/shampoos/stddef.h
@@ -3,6 +3,10 @@
 
 typedef long unsigned int size_t;
 
-#define offsetof(TYPE, MEMBER) ((size_t) & ((TYPE *)0)->MEMBER ) 
+#define offsetof(TYPE, MEMBER) ((size_t) & ((TYPE *)0)->MEMBER )
+
+#define container_of(ptr,type,member) \
+	({const typeof(((type *)0) -> member) * __mptr = (ptr);	\
+	(type *)((char *)__mptr - offsetof(type,member));	})
 
 #endif
