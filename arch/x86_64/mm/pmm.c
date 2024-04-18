3158093c03e39086bca47b3664eb1c29aa7a5940
ZhiyuanSue <2262387848@qq.com>
2024-04-18 12:09:08 +0800

    update

--- arch/x86_64/mm/pmm.c
+++ arch/x86_64/mm/pmm.c
@@ -1,14 +1,20 @@
 #include <arch/x86_64/mm/pmm.h>
+#ifdef	LOG
+#include <modules/log/log.h>
+#endif
 void arch_init_pmm(struct setup_info* arch_setup_info)
 {
 	struct multiboot_info* mtb_info = \
 		GET_MULTIBOOT_INFO(arch_setup_info);
+#ifdef	LOG
 	pr_info("mtb info addr is %x\n",(u64)mtb_info);
+#endif
 	if( MULTIBOOT_INFO_FLAG_CHECK(mtb_info->flags,MULTIBOOT_INFO_FLAG_MEM) )
 	{
 		pr_info("the mem lower is 0x%x ,the upper is 0x%x\n",mtb_info->mem.mem_lower,mtb_info->mem.mem_upper);
 	}else{
 		pr_info("no mem info\n");
+		return;
 	}
 	if(MULTIBOOT_INFO_FLAG_CHECK(mtb_info->flags,MULTIBOOT_INFO_FLAG_MMAP))
 	{
--- include/arch/x86_64/mm/pmm.h
+++ include/arch/x86_64/mm/pmm.h
@@ -6,8 +6,6 @@
 
 #include <arch/x86_64/boot/multiboot.h>
 #include <arch/x86_64/boot/arch_setup.h>
-#include <modules/log/log.h>
-
 
 void arch_init_pmm(struct setup_info* arch_setup_info);
 
