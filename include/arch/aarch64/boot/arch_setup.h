cd485b04c12ece9ae3fdfb71be37847215f102ce
ZhiyuanSue <2262387848@qq.com>
2024-05-03 12:29:51 +0800

    add arch setup info for aarch64

---
+++ include/arch/aarch64/boot/arch_setup.h
@@ -0,0 +1,9 @@
+#ifndef _SHAMPOOS_ARCH_SETUP_H_
+#define _SHAMPOOS_ARCH_SETUP_H_
+#include <common/types.h>
+struct setup_info{
+	u64	dtb_ptr;
+} __attribute__((packed));
+
+
+#endif
---
+++ include/arch/aarch64/mm/pmm.h
@@ -0,0 +1,6 @@
+#ifndef _SHAMPOOS_ARCH_PMM_H_
+#define _SHAMPOOS_ARCH_PMM_H_
+
+
+#include <arch/aarch64/boot/arch_setup.h>
+#endif
