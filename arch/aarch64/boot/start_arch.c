9fb1f6a09b4fcdabc78c1431d5071e9b78c19c37
ZhiyuanSue <2262387848@qq.com>
2024-05-18 00:09:34 +0800

    redesign dtb map

--- arch/aarch64/boot/start_arch.c
+++ arch/aarch64/boot/start_arch.c
@@ -1,51 +1,39 @@
 #include <arch/aarch64/boot/arch_setup.h>
 #include <arch/aarch64/power_ctrl.h>
+#include <arch/aarch64/mm/page_table_def.h>
+#include <arch/aarch64/mm/vmm.h>
 #include <modules/log/log.h>
 #include <modules/dtb/dtb.h>
 #include <common/mm.h>
-void	try_map_dtb(struct fdt_header* dtb_header_ptr,struct setup_info* arch_setup_info)
+extern	u64	L2_table;
+static void map_dtb(struct setup_info* arch_setup_info)
 {
-	/*calculate the dtb size
-		case 1:if the dtb size is less or equal than 4k,do nothing
-		case 2:if the left space of this 2m entry can store the left part of this dtb
-		we should map the left pages to it
-		case 3:if the left space is larger then 2m, but less then the 1G - ( map_end - round_down(map_end,1G) )
-		we should map the left pages with 2m pages,and we also remap the header
-		case 4:if not the upper case, which means that the dtb file is too large or cross the 1G boundary
-		we just generate an error
-		after that we should update the dtb base addr if necessary
+	/*
+		map the dtb, using the linux boot protocol, which define that:
+		the dtb must be 8 byte align, and less then 2m
+		as it haven't defined that dtb must 2m align, we must alloc 4m
 	*/
-	u32	dtb_size=dtb_header_ptr->totalsize;
-	pr_info("dtb size is 0x%x\n",dtb_size);
-	if(dtb_size<PAGE_SIZE)	/*case 1*/
-		return;
-	u32	unmapped_size=dtb_size-PAGE_SIZE;
-	u32	left_space_of_2m_entry	=	ROUND_UP(((u64)dtb_header_ptr+PAGE_SIZE),MIDDLE_PAGE_SIZE) - (u64)dtb_header_ptr;
-	u32	left_space_of_1g_entry	=	ROUND_UP(((u64)dtb_header_ptr+PAGE_SIZE),HUGE_PAGE_SIZE) - ROUND_UP(((u64)dtb_header_ptr+PAGE_SIZE),MIDDLE_PAGE_SIZE);
-	if(unmapped_size<=left_space_of_2m_entry){	/*case 2*/
-
-		return;
-	}else if(dtb_size<=left_space_of_1g_entry){	/*case 3*/
-
-		arch_setup_info->boot_dtb_header_base_addr=ROUND_UP(((u64)dtb_header_ptr+PAGE_SIZE),MIDDLE_PAGE_SIZE);
-		return;
-	}
-	/*case 4*/
-	pr_error("error at map dtb\n");
-	arch_shutdown();
+	u64	vaddr=ROUND_UP(arch_setup_info->map_end_virt_addr,MIDDLE_PAGE_SIZE);
+	u64	paddr=ROUND_DOWN(arch_setup_info->dtb_ptr,MIDDLE_PAGE_SIZE);
+	arch_set_L2_entry_huge(paddr,vaddr,&L2_table,(PT_DESC_V|PT_DESC_ATTR_LOWER_AF));
+	vaddr+=MIDDLE_PAGE_SIZE;
+	paddr+=MIDDLE_PAGE_SIZE;
+	arch_set_L2_entry_huge(paddr,vaddr,&L2_table,(PT_DESC_V|PT_DESC_ATTR_LOWER_AF));
+	u64	offset=vaddr-paddr;
+	arch_setup_info->boot_dtb_header_base_addr=arch_setup_info->dtb_ptr+offset;
+	arch_setup_info->map_end_virt_addr=arch_setup_info->boot_dtb_header_base_addr+MIDDLE_PAGE_SIZE*2;
 }
 int	start_arch (struct setup_info* arch_setup_info)
 {
-	
+	/*parse the dtb*/
+	map_dtb(arch_setup_info);
+	pr_info("dtb addr is 0x%x\n",arch_setup_info->boot_dtb_header_base_addr);
 	pr_info("dtb phy addr is 0x%x\n",arch_setup_info->dtb_ptr);
 	/*we need to map the dtb page to a virt address first*/
 	pr_info("map end addr is 0x%x\n",arch_setup_info->map_end_virt_addr);
 	pr_info("uart addr is 0x%x\n",arch_setup_info->boot_uart_base_addr);
-	pr_info("dtb addr is 0x%x\n",arch_setup_info->boot_dtb_header_base_addr);
-
-	/*parse the dtb*/
 	struct fdt_header* dtb_header_ptr = (struct fdt_header*)arch_setup_info->boot_dtb_header_base_addr;
-	try_map_dtb(dtb_header_ptr,arch_setup_info);
+	pr_info("dtb length is 0x%x\n",dtb_header_ptr->totalsize);
 	
 	return 0;
 }
