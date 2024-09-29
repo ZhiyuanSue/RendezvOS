6b6a2190630e4190c0059bef4107237435fe8e93
ZhiyuanSue <2262387848@qq.com>
2024-09-29 14:04:50 +0800

    update test

--- modules/test/test.c
+++ modules/test/test.c
@@ -4,7 +4,7 @@ static struct test_case all_test[MAX_TEST_CASE] = {
         {rb_tree_test, "rb_tree\0"},
         {arch_vmm_test, "arch_vmm\0"},
         {nexus_test, "nexus\0"},
-		/*the pmm test will use almost all of the memory frame, so it must put
+        /*the pmm test will use almost all of the memory frame, so it must put
          * at the end*/
         {pmm_test, "pmm\0"},
 };
@@ -12,12 +12,16 @@ static struct test_case all_test[MAX_TEST_CASE] = {
 void test(void)
 {
         pr_info("====== [ KERNEL TEST ] ======\n");
-		for(int i=0;i<MAX_TEST_CASE;i++){
-			if(all_test[i].test()){
-				pr_error("[ TEST ERROR ] test %s fail!\n",all_test[i].name);
-				break;
-			}else{
-				pr_info("[ TEST PASS ] test %s pass!\n",all_test[i].name);
-			}
-		}
+        for (int i = 0; i < MAX_TEST_CASE; i++) {
+                if ((u64)(all_test[i].test)) {
+                        if (all_test[i].test()) {
+                                pr_error("[ TEST ] ERROR: test %s fail!\n",
+                                         all_test[i].name);
+                                break;
+                        } else {
+                                pr_info("[ TEST ] PASS: test %s ok!\n",
+                                        all_test[i].name);
+                        }
+                }
+        }
 }
