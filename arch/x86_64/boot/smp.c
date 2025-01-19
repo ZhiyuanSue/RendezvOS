32c1d12403b787174f54e70f0ba568b1653e5c24
ZhiyuanSue <2262387848@qq.com>
2025-01-19 09:10:28 +0800

    copy ap_start code and try to start the aps

--- arch/x86_64/boot/smp.c
+++ arch/x86_64/boot/smp.c
@@ -1,7 +1,23 @@
 #include <arch/x86_64/smp.h>
 #include <shampoos/time.h>
+#include <modules/log/log.h>
+#include <common/string.h>
+extern char ap_start;
+extern char ap_start_end;
+
+static void copy_ap_start_code()
+{
+        char* dest_ap_start_ptr =
+                (char*)KERNEL_PHY_TO_VIRT(_SHAMPOOS_X86_64_AP_PHY_ADDR_);
+        char* src_ap_start_ptr = &ap_start;
+        memcpy(dest_ap_start_ptr,
+               src_ap_start_ptr,
+               (vaddr)&ap_start_end - (vaddr)&ap_start);
+}
+
 void arch_start_smp(void)
 {
+        copy_ap_start_code();
         APIC_send_IPI(APIC_ICR_DEST_SH_ALL_EXCLUDE_SELF,
                       APIC_ICR_DEL_MODE_INIT,
                       0,
@@ -12,7 +28,15 @@ void arch_start_smp(void)
         mdelay(10);
         APIC_send_IPI(APIC_ICR_DEST_SH_ALL_EXCLUDE_SELF,
                       APIC_ICR_DEL_MODE_START_UP,
+                      PPN(_SHAMPOOS_X86_64_AP_PHY_ADDR_),
                       0,
+                      APIC_ICR_LEVEL,
+                      0,
+                      0);
+        udelay(200);
+        APIC_send_IPI(APIC_ICR_DEST_SH_ALL_EXCLUDE_SELF,
+                      APIC_ICR_DEL_MODE_START_UP,
+                      PPN(_SHAMPOOS_X86_64_AP_PHY_ADDR_),
                       0,
                       APIC_ICR_LEVEL,
                       0,
--- include/arch/x86_64/smp.h
+++ include/arch/x86_64/smp.h
@@ -1,5 +1,9 @@
 #ifndef _SHAMPOOS_ARCH_SMP_H_
 #define _SHAMPOOS_ARCH_SMP_H_
 #include <arch/x86_64/PIC/LocalAPIC.h>
+#define _SHAMPOOS_X86_64_AP_PHY_ADDR_ 0x1000
+/*
+    we put the ap entry code at 0x1000 when we try to mp set up
+*/
 void arch_start_smp(void);
 #endif
