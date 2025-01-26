3bd51e7ab3085f409bbfc52cb2e92acafcd7bffc
ZhiyuanSue <2262387848@qq.com>
2025-01-26 21:54:58 +0800

    fill in all the 64bit psci calls

--- include/modules/psci/psci.h
+++ include/modules/psci/psci.h
@@ -5,6 +5,7 @@
 #include <modules/dtb/dtb.h>
 #include <modules/log/log.h>
 #include "psci_error.h"
+/*although I think the 32 version will not be used, I still record it*/
 enum psci_call_method { psci_call_none = 0, psci_call_smc, psci_call_hvc };
 void psci_init(void);
 
@@ -31,30 +32,74 @@ i32 psci_affinity_info_32(u32 target_affinity, u32 lowest_affinity_level);
 #define psci_64_affinity_info_func_id 0xC4000004
 i64 psci_affinity_info_64(u64 target_affinity, u32 lowest_affinity_level);
 
-#define psci_32_migrate_function_id 0x84000005
+#define psci_32_migrate_func_id 0x84000005
 i32 psci_migrate_32(u32 target_cpu);
-#define psci_64_migrate_function_id 0xC4000005
+#define psci_64_migrate_func_id 0xC4000005
 i64 psci_migrate_64(u64 target_cpu);
 
-#define psci_migrate_info_type_function_id 0x84000006
+#define psci_migrate_info_type_func_id 0x84000006
 i32 psci_migrate_info_type(void);
 
-#define psci_32_migrate_info_up_cpu_function_id 0x84000007
-void psci_migrate_info_up_cpu(void);
+#define psci_32_migrate_info_up_cpu_func_id 0x84000007
+i32 psci_migrate_info_up_cpu_32(void);
+#define psci_64_migrate_info_up_cpu_func_id 0xC4000007
+i64 psci_migrate_info_up_cpu_64(void);
 
-#define psci_system_off_function_id 0x84000008
+#define psci_system_off_func_id 0x84000008
 void psci_system_off(void);
-void psci_system_off_2(void);
+
+#define psci_32_system_off_2_func_id 0x84000015
+i32 psci_system_off_2_32(u32 type, u32 cookie);
+#define psci_64_system_off_2_func_id 0xC4000015
+i64 psci_system_off_2_64(u32 type, u64 cookie);
+
+#define psci_system_reset_func_id 0x84000009
 void psci_system_reset(void);
-void psci_system_reset_2(void);
-void psci_mem_protect(void);
-void psci_mem_protect_check_range(void);
-void psci_feature(void);
-void psci_freeze(void);
-void psci_cpu_default_suspend(void);
-void psci_node_hw_state(void);
-void psci_system_suspend(void);
-void psci_set_suspend_mode(void);
-void psci_state_residency(void);
-void psci_state_count(void);
+
+#define psci_32_system_reset_2_func_id 0x84000012
+i32 psci_system_reset_2_32(u32 reset_type, u32 cookie);
+#define psci_64_system_reset_2_func_id 0xC4000012
+i64 psci_system_reset_2_64(u32 reset_type, u64 cookie);
+
+#define psci_mem_protect_func_id 0x84000013
+i32 psci_mem_protect(u32 enable);
+
+#define psci_32_mem_protect_check_range_func_id 0x84000014
+i32 psci_mem_protect_check_range_32(u32 base, u32 length);
+#define psci_64_mem_protect_check_range_func_id 0xC4000014
+i64 psci_mem_protect_check_range_64(u64 base, u64 length);
+
+#define psci_feature_function_id 0x8400000A
+i32 psci_feature(u32 psci_feature_id);
+
+#define psci_cpu_freeze_func_id 0x8400000B
+i32 psci_cpu_freeze(void);
+
+#define psci_32_cpu_default_suspend_func_id 0x8400000C
+i32 psci_cpu_default_suspend_32(u32 entry_point_address, u32 context_id);
+#define psci_64_cpu_default_suspend_func_id 0xC400000C
+i64 psci_cpu_default_suspend_64(u64 entry_point_address, u64 context_id);
+
+#define psci_32_node_hw_state_func_id 0x8400000D
+i32 psci_node_hw_state_32(u32 target_cpu, u32 power_level);
+#define psci_64_node_hw_state_func_id 0xC400000D
+i64 psci_node_hw_state_64(u64 target_cpu, u32 power_level);
+
+#define psci_32_system_suspend_func_id 0x8400000E
+i32 psci_system_suspend_32(u32 entry_point_address, u32 context_id);
+#define psci_64_system_suspend_func_id 0xC400000E
+i64 psci_system_suspend_64(u64 entry_point_address, u64 context_id);
+
+#define psci_set_suspend_mode_func_id 0x8400000F
+i32 psci_set_suspend_mode(u32 mode);
+
+#define psci_32_state_residency_func_id 0x84000010
+u32 psci_state_residency_32(u32 target_cpu, u32 power_state);
+#define psci_64_state_residency_func_id 0xC4000010
+u64 psci_state_residency_64(u64 target_cpu, u32 power_state);
+
+#define psci_32_state_count_func_id 0x84000011
+u32 psci_state_count_32(u32 target_cpu, u32 power_state);
+#define psci_64_state_count_func_id 0xC4000011
+u64 psci_state_count_64(u64 target_cpu, u32 power_state);
 #endif
--- modules/psci/psci.c
+++ modules/psci/psci.c
@@ -81,14 +81,6 @@ i32 psci_cpu_off(void)
         }
         return is_denied;
 }
-void psci_system_off(void)
-{
-        if (psci_call_type == psci_call_smc) {
-                psci_smc(psci_system_off_function_id, 0, 0, 0);
-        } else if (psci_call_type == psci_call_hvc) {
-                psci_hvc(psci_system_off_function_id, 0, 0, 0);
-        }
-}
 
 i64 psci_cpu_on_64(u64 target_cpu, u64 entry_point_address, u64 context_id)
 {
@@ -103,6 +95,262 @@ i64 psci_cpu_on_64(u64 target_cpu, u64 entry_point_address, u64 context_id)
                                target_cpu,
                                entry_point_address,
                                context_id);
+        }
+        return res;
+}
+
+i64 psci_cpu_suspend_64(u32 power_state, u64 entry_point_address,
+                        u32 context_id)
+{
+        i64 res = 0;
+        if (psci_call_type == psci_call_smc) {
+                res = psci_smc(psci_64_cpu_suspend_func_id,
+                               power_state,
+                               entry_point_address,
+                               context_id);
+        } else if (psci_call_type == psci_call_hvc) {
+                res = psci_hvc(psci_64_cpu_suspend_func_id,
+                               power_state,
+                               entry_point_address,
+                               context_id);
+        }
+        return res;
+}
+
+i64 psci_affinity_info_64(u64 target_affinity, u32 lowest_affinity_level)
+{
+        i64 res = 0;
+        if (psci_call_type == psci_call_smc) {
+                res = psci_smc(psci_64_affinity_info_func_id,
+                               target_affinity,
+                               lowest_affinity_level,
+                               0);
+        } else if (psci_call_type == psci_call_hvc) {
+                res = psci_hvc(psci_64_affinity_info_func_id,
+                               target_affinity,
+                               lowest_affinity_level,
+                               0);
+        }
+        return res;
+}
+
+i64 psci_migrate_64(u64 target_cpu)
+{
+        i64 res = 0;
+        if (psci_call_type == psci_call_smc) {
+                res = psci_smc(psci_64_migrate_func_id, target_cpu, 0, 0);
+        } else if (psci_call_type == psci_call_hvc) {
+                res = psci_hvc(psci_64_migrate_func_id, target_cpu, 0, 0);
+        }
+        return res;
+}
+
+i32 psci_migrate_info_type(void)
+{
+        i32 res = 0;
+        if (psci_call_type == psci_call_smc) {
+                res = psci_smc(psci_migrate_info_type_func_id, 0, 0, 0);
+        } else if (psci_call_type == psci_call_hvc) {
+                res = psci_hvc(psci_migrate_info_type_func_id, 0, 0, 0);
+        }
+        return res;
+}
+
+i64 psci_migrate_info_up_cpu_64(void)
+{
+        i64 res = 0;
+        if (psci_call_type == psci_call_smc) {
+                res = psci_smc(psci_64_migrate_info_up_cpu_func_id, 0, 0, 0);
+        } else if (psci_call_type == psci_call_hvc) {
+                res = psci_hvc(psci_64_migrate_info_up_cpu_func_id, 0, 0, 0);
+        }
+        return res;
+}
+
+void psci_system_off(void)
+{
+        if (psci_call_type == psci_call_smc) {
+                psci_smc(psci_system_off_func_id, 0, 0, 0);
+        } else if (psci_call_type == psci_call_hvc) {
+                psci_hvc(psci_system_off_func_id, 0, 0, 0);
+        }
+}
+
+i64 psci_system_off_2_64(u32 type, u64 cookie)
+{
+        i64 res = 0;
+        if (psci_call_type == psci_call_smc) {
+                res = psci_smc(psci_64_system_off_2_func_id, type, cookie, 0);
+        } else if (psci_call_type == psci_call_hvc) {
+                res = psci_hvc(psci_64_system_off_2_func_id, type, cookie, 0);
+        }
+        return res;
+}
+
+void psci_system_reset(void)
+{
+        if (psci_call_type == psci_call_smc) {
+                psci_smc(psci_system_reset_func_id, 0, 0, 0);
+        } else if (psci_call_type == psci_call_hvc) {
+                psci_hvc(psci_system_reset_func_id, 0, 0, 0);
+        }
+}
+
+i64 psci_system_reset_2_64(u32 reset_type, u64 cookie)
+{
+        i64 res = 0;
+        if (psci_call_type == psci_call_smc) {
+                res = psci_smc(
+                        psci_64_system_reset_2_func_id, reset_type, cookie, 0);
+        } else if (psci_call_type == psci_call_hvc) {
+                res = psci_hvc(
+                        psci_64_system_reset_2_func_id, reset_type, cookie, 0);
+        }
+        return res;
+}
+
+i32 psci_mem_protect(u32 enable)
+{
+        i32 res = 0;
+        if (psci_call_type == psci_call_smc) {
+                res = psci_smc(psci_mem_protect_func_id, enable, 0, 0);
+        } else if (psci_call_type == psci_call_hvc) {
+                res = psci_hvc(psci_mem_protect_func_id, enable, 0, 0);
+        }
+        return res;
+}
+
+i64 psci_mem_protect_check_range_64(u64 base, u64 length)
+{
+        i64 res = 0;
+        if (psci_call_type == psci_call_smc) {
+                res = psci_smc(psci_64_mem_protect_check_range_func_id,
+                               base,
+                               length,
+                               0);
+        } else if (psci_call_type == psci_call_hvc) {
+                res = psci_hvc(psci_64_mem_protect_check_range_func_id,
+                               base,
+                               length,
+                               0);
+        }
+        return res;
+}
+
+i32 psci_feature(u32 psci_feature_id)
+{
+        i32 res = 0;
+        if (psci_call_type == psci_call_smc) {
+                res = psci_smc(psci_feature_function_id, psci_feature_id, 0, 0);
+        } else if (psci_call_type == psci_call_hvc) {
+                res = psci_hvc(psci_feature_function_id, psci_feature_id, 0, 0);
+        }
+        return res;
+}
+
+i32 psci_cpu_freeze(void)
+{
+        i32 res = 0;
+        if (psci_call_type == psci_call_smc) {
+                res = psci_smc(psci_cpu_freeze_func_id, 0, 0, 0);
+        } else if (psci_call_type == psci_call_hvc) {
+                res = psci_hvc(psci_cpu_freeze_func_id, 0, 0, 0);
+        }
+        return res;
+}
+
+i64 psci_cpu_default_suspend_64(u64 entry_point_address, u64 context_id)
+{
+        i64 res = 0;
+        if (psci_call_type == psci_call_smc) {
+                res = psci_smc(psci_64_cpu_default_suspend_func_id,
+                               entry_point_address,
+                               context_id,
+                               0);
+        } else if (psci_call_type == psci_call_hvc) {
+                res = psci_hvc(psci_64_cpu_default_suspend_func_id,
+                               entry_point_address,
+                               context_id,
+                               0);
+        }
+        return res;
+}
+
+i64 psci_node_hw_state_64(u64 target_cpu, u32 power_level)
+{
+        i64 res = 0;
+        if (psci_call_type == psci_call_smc) {
+                res = psci_smc(psci_64_node_hw_state_func_id,
+                               target_cpu,
+                               power_level,
+                               0);
+        } else if (psci_call_type == psci_call_hvc) {
+                res = psci_hvc(psci_64_node_hw_state_func_id,
+                               target_cpu,
+                               power_level,
+                               0);
+        }
+        return res;
+}
+
+i64 psci_system_suspend_64(u64 entry_point_address, u64 context_id)
+{
+        i64 res = 0;
+        if (psci_call_type == psci_call_smc) {
+                res = psci_smc(psci_64_system_suspend_func_id,
+                               entry_point_address,
+                               context_id,
+                               0);
+        } else if (psci_call_type == psci_call_hvc) {
+                res = psci_hvc(psci_64_system_suspend_func_id,
+                               entry_point_address,
+                               context_id,
+                               0);
+        }
+        return res;
+}
+
+i32 psci_set_suspend_mode(u32 mode)
+{
+        i32 res = 0;
+        if (psci_call_type == psci_call_smc) {
+                res = psci_smc(psci_set_suspend_mode_func_id, mode, 0, 0);
+        } else if (psci_call_type == psci_call_hvc) {
+                res = psci_hvc(psci_set_suspend_mode_func_id, mode, 0, 0);
+        }
+        return res;
+}
+
+u64 psci_state_residency_64(u64 target_cpu, u32 power_state)
+{
+        u64 res = 0;
+        if (psci_call_type == psci_call_smc) {
+                res = psci_smc(psci_64_state_residency_func_id,
+                               target_cpu,
+                               power_state,
+                               0);
+        } else if (psci_call_type == psci_call_hvc) {
+                res = psci_hvc(psci_64_state_residency_func_id,
+                               target_cpu,
+                               power_state,
+                               0);
+        }
+        return res;
+}
+
+u64 psci_state_count_64(u64 target_cpu, u32 power_state)
+{
+        u64 res = 0;
+        if (psci_call_type == psci_call_smc) {
+                res = psci_smc(psci_64_state_count_func_id,
+                               target_cpu,
+                               power_state,
+                               0);
+        } else if (psci_call_type == psci_call_hvc) {
+                res = psci_hvc(psci_64_state_count_func_id,
+                               target_cpu,
+                               power_state,
+                               0);
         }
         return res;
 }
