375c09158e359379563178731fdee9255b4f3248
ZhiyuanSue <2262387848@qq.com>
2025-07-09 18:31:59 +0800

    try to add the pci allocate the bridge id

--- modules/pci/pci.c
+++ modules/pci/pci.c
@@ -1,5 +1,24 @@
 #include <modules/pci/pci.h>
-
+int next_bus_number;
+int recursion_depth;
+/*TODO: enable device, alloc irq, configure bar*/
+bool is_pci_bridge(pci_common_header_t *common)
+{
+        return common->class_code == 0x06;
+}
+bool pci_bridge_need_scan(pci_common_header_t *common){
+        return common->class_code == 0x06 && (common->subclass == 0x04|| common->subclass == 0x07);
+}
+void configure_pci_bridge_bus(u8 bus, u8 device, u8 func, u8 primary,
+                              u8 secondary, u8 subordinate)
+{
+        pci_config_write_IO_dword(bus,
+                                  device,
+                                  func,
+                                  0x18,
+                                  ((subordinate << 16) | (secondary << 8)
+                                   | (primary)));
+}
 error_t pci_scan_device(pci_scan_callback callback, u8 bus, u8 device, u8 func,
                         pci_header_t *header, pci_common_header_t *common)
 {
@@ -8,45 +27,29 @@ error_t pci_scan_device(pci_scan_callback callback, u8 bus, u8 device, u8 func,
         for (int i = 0; i < 4; i++) {
                 ptr[i] = pci_config_read_IO_dword(bus, device, 0, i * 4);
         }
-        u8 header_type = common->header_type & 0x7F;
+        for (int i = 4; i < 16; i++) {
+                ptr[i] = pci_config_read_IO_dword(bus, device, 0, i * 4);
+        }
+        if (pci_bridge_need_scan(common)) {
+                u8 new_secondary = next_bus_number++;
+                configure_pci_bridge_bus(
+                        bus, device, func, bus, new_secondary, 0xFF);
+                recursion_depth ++;
+                u8 bus_before = next_bus_number;
+                e = pci_scan_bus(callback, new_secondary);
+                recursion_depth --;
+                u8 new_subordinate = next_bus_number - 1;
 
-        switch (header_type) {
-        case 0x00:
-                for (int i = 4; i < 16; i++) {
-                        ptr[i] =
-                                pci_config_read_IO_dword(bus, device, 0, i * 4);
-                }
-                e = callback(bus, device, 0, header);
-                break;
-
-        case 0x01:
-                /*
-                        TODO : we need to add the bridge bus id allocation,
-                        to avoid infinite recursion
-                        beside, we need to write into the pci space
-                */
-                for (int i = 4; i < 16; i++) {
-                        ptr[i] =
-                                pci_config_read_IO_dword(bus, device, 0, i * 4);
-                }
-                e = callback(bus, device, 0, header);
-
+                configure_pci_bridge_bus(
+                        bus, device, func, bus, new_secondary, new_subordinate);
                 if (e)
                         return e;
 
-                e = pci_scan_bus(callback, header->type1.secondary_bus);
-                break;
-
-        case 0x02:
-                for (int i = 4; i < 16; i++) {
-                        ptr[i] =
-                                pci_config_read_IO_dword(bus, device, 0, i * 4);
-                }
+                /*need to generate some data structure*/
                 e = callback(bus, device, 0, header);
-                break;
 
-        default:
-                break;
+        } else {
+                e = callback(bus, device, 0, header);
         }
         return e;
 }
@@ -77,8 +80,9 @@ error_t pci_scan_bus(pci_scan_callback callback, u8 bus)
 }
 error_t pci_scan_all(pci_scan_callback callback)
 {
-        for (int bus = 0; bus < PCI_MAX_BUS; bus++) {
-                pci_scan_bus(callback, bus);
-        }
+        next_bus_number = 1;
+        recursion_depth = 0;
+        /*we only scan the bus 0 and other bus will be scanned from bus 0*/
+        pci_scan_bus(callback, 0);
         return 0;
 }
