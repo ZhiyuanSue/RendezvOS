2179f772d8ad7e735c4e0bb75a857e9557e92c3d
ZhiyuanSue <2262387848@qq.com>
2025-08-03 13:27:57 +0800

    add some of the comment

--- include/common/dsa/lock_free_list.h
+++ include/common/dsa/lock_free_list.h
@@ -8,4 +8,15 @@
 
 /*a lock free implement*/
 
+/*
+we should at least include 4 ops:
+ - init a empty linked list
+ - enqueue
+ - get the snapshot
+ - dequeue
+ - others(for each ?)
+the writer enqueue the list
+the reader get the snapshot and use for each to dequeue
+*/
+
 #endif
