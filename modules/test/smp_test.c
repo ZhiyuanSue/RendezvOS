52ff26441e4af417a6e09a6fdf7372928584489a
ZhiyuanSue <2262387848@qq.com>
2026-01-05 00:31:17 +0800

    fix bug

--- modules/test/smp_test.c
+++ modules/test/smp_test.c
@@ -9,7 +9,7 @@ static struct smp_test_case smp_test[MAX_SMP_TEST_CASE] = {
         {smp_lock_test, "smp spin_lock", smp_lock_check},
         // {smp_log_test, "smp log test", smp_log_check}, // just see the output
         {smp_nexus_test, "smp nexus test", NULL},
-        {smp_spmalloc_test, "smp spmalloc", NULL},
+        {smp_kmalloc_test, "smp spmalloc", NULL},
         {smp_ms_queue_test, "smp ms queue test", smp_ms_queue_check},
         {smp_ms_queue_dyn_alloc_test,
          "smp ms queue dyn alloc test",
